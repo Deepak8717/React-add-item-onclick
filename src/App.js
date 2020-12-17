e169fa1b22dedad7809531aaaba058b9096dd78a
Deepak pundir <sendit2deepak@gmail.com>
2020-12-17 22:38:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,21 @@
 import React from "react";
-import data from "./data";
+import { render } from "react-dom";
 import "./App.css";
 
 class App extends React.Component {
   state = {
-    dishes: data.dishes,
-    dishIndex: 0,
+    color: "red",
   };
 
-  prev = () => {
-    this.setState((prevState) => {
-      return {
-        dishIndex: prevState.dishIndex - 1,
-      };
-    });
+  toggleClass = () => {
+    this.setState({ color: this.state.color === "red" ? "blue" : "red" });
   };
-  next = () => {
-    this.setState((prevState) => {
-      return {
-        dishIndex: prevState.dishIndex + 1,
-      };
-    });
-  };
+
   render() {
-    const { dishIndex, dishes } = this.state;
-    const rowStyle = {
-      transform: `translateX(-${dishIndex * (100 / dishes.length)}%)`,
-    };
     return (
-      <div className="App">
-        <div className="wrapper">
-          <div className="row" style={rowStyle}>
-            {dishes.map((dish) => (
-              <div className="slide" key={dish.index}>
-                <img src={dish.url} alt="A Dish" />
-              </div>
-            ))}
-          </div>
-        </div>
-        <button onClick={this.prev} disabled={dishIndex === 0}>
-          Prev
-        </button>
-        <button onClick={this.next} disabled={dishIndex === dishes.length - 1}>
-          Next
-        </button>
+      <div>
+        <p className={`${this.state.color}`}>click toggle to change colors</p>
+        <button onClick={this.toggleClass}>toggle</button>
       </div>
     );
   }

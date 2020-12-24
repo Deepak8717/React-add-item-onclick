867c3745d69cb9a6769bde951765192564de46bb
Deepak pundir <sendit2deepak@gmail.com>
2020-12-24 23:27:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,30 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import posts from "./posts.json";
 
-const Layout = (props) => (
-  <div>
-    <h1>Bender's Most awesome blog</h1>
-    <hr />
-    <div>{props.children}</div>
-  </div>
-);
+const styles = {
+  fontSize: 48,
+  background: "red",
+  margin: "40px",
+  padding: "40 40",
+  borderRadius: "4px",
+};
 
-const PostsList = ({ posts }) => (
-  <div>
-    <ul>
-      {posts.map((post) => (
-        <li>
-          <Link to={`/post/${post.id}`}>
-            <h2>{post.title}</h2>
-          </Link>
-          <p>
-            Published by <strong>{post.author}</strong>on{post.publishedAt}
-          </p>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
-
-const PostPage = ({ post }) => (
-  <div>
-    <h2>{post.title}</h2>
-    <div>{post.content}</div>
-    <p>
-      <Link to="/">Back</Link>
-    </p>
-  </div>
-);
-
-const App = () => (
-  <Router>
-    <Layout>
-      <Switch>
-        <Route exact path="/" render={() => <PostsList posts={posts} />} />
-        <Route
-          path="/post/:id"
-          render={({ match }) => <PostPage post={posts[match.params.id - 1]} />}
-        />
-        <Route render={() => <div>Post not found</div>} />
-      </Switch>
-    </Layout>
-  </Router>
-);
+class App extends React.Component {
+  state = {
+    isToggleOn: true,
+  };
+  handleClick = () => {
+    this.setState((prevState) => ({
+      isToggleOn: !prevState.isToggleOn,
+    }));
+  };
+  render() {
+    return (
+      <button style={styles} onClick={this.handleClick}>
+        {this.state.isToggleOn ? "On" : "Off"}
+      </button>
+    );
+  }
+}
 
 export default App;

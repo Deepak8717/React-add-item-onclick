cda7fa0458ba594bd893958b29cd338cb34789fc
Deepak pundir <sendit2deepak@gmail.com>
2021-01-10 20:25:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 import React from "react";
-import Counter from "./Counter";
 import "./App.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super();
-  }
+  state = {
+    rows: [{}],
+  };
+  handleAddRow = () => {
+    const item = {
+      name: "",
+      mobile: "",
+    };
+    this.setState({
+      rows: [...this.state.rows, item],
+    });
+  };
+
   render() {
     return (
       <div>
-        <Counter />
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <table>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Mobile</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.rows.map((item, idx) => (
+                    <tr id="addr0" key={idx}>
+                      <td>{idx}</td>
+                      <td>
+                        <input type="text" name="name" />
+                      </td>
+                      <td>
+                        <input type="text" name="mobile" />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <button onClick={this.handleAddRow}>Add Row</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

7a818f846cb44560129a23bd51e9a152c1d26623
Deepak pundir <sendit2deepak@gmail.com>
2021-01-11 19:15:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
+
 import { render } from "react-dom";
 
-export default class App extends Component {
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center",
+};
+
+export default class App extends React.Component {
   state = {
-    beers: [],
+    people: [],
   };
 
-  fetchBeers = (param) => {
-    fetch(`https://api.punkapi.com/v2/beers?${param}`)
-      .then((data) => data.json())
-      .then((beers) => {
-        this.setState({ beers });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  loadData = async () => {
+    const response = await fetch("https://randomuser.me/api/?results=10");
+    const data = await response.json();
+    this.setState({
+      people: data.results,
+    });
   };
   render() {
-    let beers = this.state.beers.map((beer) => <p>{beer.name}</p>);
+    const { people } = this.state;
     return (
-      <div>
-        <button onClick={this.fetchBeers}>All</button>
-        <button onClick={() => this.fetchBeers("food=taco")}>
-          Beers for taco !
-        </button>
-        <section>{beers}</section>
+      <div style={styles}>
+        <button onClick={this.loadData}>Load Data</button>
+        {people.map((x) => (
+          <div key={x.name.first}>{x.name.first}</div>
+        ))}
       </div>
     );
   }

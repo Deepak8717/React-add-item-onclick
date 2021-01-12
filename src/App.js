3f0969dcf197a2319ecfd1eaf59e49b5f61ffeb3
Deepak pundir <sendit2deepak@gmail.com>
2021-01-12 14:03:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import React from "react";
-
+import React, { Component } from "react";
 import { render } from "react-dom";
 
-const styles = {
-  fontFamily: "sans-serif",
-  textAlign: "center",
-};
+import Background from "./Background";
 
 export default class App extends React.Component {
-  state = {
-    people: [],
-  };
-
-  loadData = async () => {
-    const response = await fetch("https://randomuser.me/api/?results=10");
-    const data = await response.json();
-    this.setState({
-      people: data.results,
-    });
-  };
   render() {
-    const { people } = this.state;
     return (
-      <div style={styles}>
-        <button onClick={this.loadData}>Load Data</button>
-        {people.map((x) => (
-          <div key={x.name.first}>{x.name.first}</div>
-        ))}
+      <div>
+        <Background />
       </div>
     );
   }

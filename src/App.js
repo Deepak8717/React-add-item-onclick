588ed157dac5adcc5b7cba4efceb443e817be808
Deepak pundir <sendit2deepak@gmail.com>
2021-01-13 19:44:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import React, { useState } from "react";
 
-import Background from "./Background";
+function App() {
+  const [responses, setResponses] = useState([]);
+  const [response, setResponse] = useState("");
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Background />
-      </div>
-    );
+  function addResponse() {
+    setResponses([...responses, { name: response }]);
   }
+  function deleteResponse(itemIndex) {
+    const newResponses = responses.filter((item, index) => index !== itemIndex);
+    setResponses(newResponses);
+  }
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <div>
+        <input
+          name="response"
+          onChange={(e) => {
+            setResponse(e.target.value);
+          }}
+          value={response}
+        />
+        <button onClick={addResponse}>Add</button>
+        {responses.map(({ name }, index) => (
+          <div>
+            <span>{name}</span>
+            <button onClick={() => deleteResponse(index)}>X</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
+export default App;
